38fd037a41bd6518eb50a021707ce0f00b82ebf5
Tadas <celsyum@gmail.com>
2023-09-30 23:54:55 +0300

    level selector has now disabled status before unlocking, show level number

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,27 +1,62 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelSelector : MonoBehaviour
 {
     public UnityEngine.Events.UnityEvent onPressed;
 
+    public GameObject disabledShield;
     public GameObject pink_button;
     public GameObject blue_button;
+    public TextMeshPro label;
+
 
     public GameObject[] points;
+    
 
     //setter for level
     public int points_count;
     public int level;
 
+    //setter for unlocked variable
+    private bool _unlocked = false;
+    public bool unlocked
+    {
+        get
+        {
+            return _unlocked;
+        }
+        set
+        {
+            foreach (var item in points)
+            {
+                item.SetActive(value);
+            }
+            disabledShield.SetActive(!value);
+
+            _unlocked = value;
+        }
+    }
+
+
+    
+
+
+
     public void PrepareButton()
     {
+        label.text = (level + 1).ToString();
+        if (!unlocked)
+        {
+            return;
+        }
         int i = 0;
         for (i = 0; i < points_count; i++)
         {
             GameObject point = Instantiate(blue_button, points[i].transform);
             point.transform.position = points[i].transform.position;
         }
-
+        
         for (int j = i; j < 3; j++)
         {
             GameObject point = Instantiate(pink_button, points[j].transform);
@@ -31,6 +66,10 @@ public class LevelSelector : MonoBehaviour
 
     void Update()
     {
+        if (!unlocked)
+        {
+            return;
+        }
         for (int i = 0; i < Input.touchCount; ++i)
         {
             if (Input.GetTouch(i).phase == TouchPhase.Began)
