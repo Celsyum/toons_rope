2fb5ceb60fcf159e9d80ee8966e2ee7b2fc45969
Tadas <celsyum@gmail.com>
2023-10-01 19:04:04 +0300

    improved clicking events, added centered option for label for hude dataset

--- Assets/Scripts/DiamondController.cs
+++ Assets/Scripts/DiamondController.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 [System.Serializable]
 public class ClickEvent : UnityEvent<DiamondController>
 {
 }
 
-public class DiamondController : MonoBehaviour
+public class DiamondController : MonoBehaviour, IPointerClickHandler
 {
     public SpriteRenderer background;
     public ClickEvent onPressed;
@@ -17,6 +18,20 @@ public class DiamondController : MonoBehaviour
     public GameObject label;
     [HideInInspector]
     public int _queue = 0;
+
+    bool _make_centered = false;
+    public bool make_centered
+    {
+        get
+        {
+            return _make_centered;
+        }
+        set
+        {
+            _make_centered = value;
+            label.GetComponentInParent<LookAtCenter>().centered = _make_centered;
+        }
+    }
 
     public int queue
     {
@@ -36,56 +51,19 @@ public class DiamondController : MonoBehaviour
         background.sprite = Sprite.Create(texturePath, new Rect(0, 0, texturePath.width, texturePath.height), new Vector2(0.5f, 0.5f), 100f);
         //tween label alpha to 0
         iTween.ValueTo(label, iTween.Hash("from", 1f, "to", 0f, "time", 1f, "onupdate", "OnLabelTweenUpdate", "oncomplete", "OnLabelTweenEnded"));
-        
+        background.GetComponent<BoxCollider>().enabled = false;
         StartCoroutine(DelayedColliderDisable());
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerClick");
+        onPressed.Invoke(this);
     }
 
     IEnumerator DelayedColliderDisable()
     {
-        yield return new WaitForSeconds(0.2f);
-        background.GetComponent<SphereCollider>().enabled = false;
-    }
-
-
-    void Update()
-    {
-        for (int i = 0; i < Input.touchCount; ++i)
-        {
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
-            {
-
-                // Construct a ray from the current touch coordinates
-                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    if (hit.transform == background.transform)
-                    {
-                        onPressed.Invoke(this);
-                    }
-
-                }
-
-
-            }
-        }
-#if UNITY_EDITOR || !UNITY_ANDROID && !UNITY_IOS 
-        //detect mouse click
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Construct a ray from the current mouse coordinates
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            // Create a particle if hit
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform == background.transform)
-                {
-                    onPressed.Invoke(this);
-                }
-            }
-        }
-#endif
+        yield return new WaitForSeconds(0.02f);
+        
     }
 }
